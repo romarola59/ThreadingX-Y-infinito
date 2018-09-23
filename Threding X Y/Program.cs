e9f274cb57ff6ed9c040a28f4c29371441cc2856
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-23 20:28:46 -0300

    ThreadingXYinfinito

--- Threding X Y/Program.cs
+++ Threding X Y/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Threading;
+
 
 namespace Threding_X_Y
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int posX = 40, posY = 10;
+            string simbolos = "^>v<";
+            byte simboloActual = 0;
+            bool terminado = false;
+            do
+            {
+                Console.Clear();
+                Console.SetCursorPosition(posX, posY);
+                Console.Write(simbolos[simboloActual]);
+                Thread.Sleep(500);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo tecla = Console.ReadKey(true);
+                    if (tecla.Key == ConsoleKey.RightArrow) posX++;
+                    if (tecla.Key == ConsoleKey.LeftArrow) posY--;
+                    if (tecla.Key == ConsoleKey.Escape) terminado = true;
+
+                }
+                simboloActual++;
+                if (simboloActual > 3) simboloActual = 0;
+            }
+            while (!terminado);
+            
         }
     }
 }
