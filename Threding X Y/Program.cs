bb674a8c6c974d733a5aee4c861b790eded99b47
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-23 19:46:01 -0300

    Agregar archivos de proyecto.

---
+++ Threding X Y/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Threding_X_Y
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
